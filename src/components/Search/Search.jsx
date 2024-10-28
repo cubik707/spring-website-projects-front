e875ec446eac5883f014561259a4658c93874dfc
Vladislava <stdemidovez@gmail.com>
2024-10-28 17:42:07 +0300

    feat: handle input changes and clear

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,16 @@
 import styles from './Search.module.css';
+import { useState } from 'react';
 
 export default function Search(props) {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleClear = () => {
+    setSearchValue('');
+  };
   return (
     <div className={styles.search}>
       <span className={`${styles.searchIcon} material-symbols-outlined`}>
@@ -11,10 +21,13 @@ export default function Search(props) {
         type='search'
         id='search-input-projects'
         placeholder='Search'
+        value={searchValue}
+        onChange={handleInputChange}
       />
       <span
         className={`${styles.clearIcon} material-symbols-outlined`}
         id='clear-search'
+        onClick={handleClear}
       >
         clear
       </span>
