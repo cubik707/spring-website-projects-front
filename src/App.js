2d1ad4284a390861cf4203ac77f535d102ac4698
Vladislava <stdemidovez@gmail.com>
2024-11-05 16:13:05 +0300

    refactor: use prettier and move ProtectedRoute to a separate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
+import { router } from './router/router';
 
 function App() {
   return (
--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,7 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/login-page/login-page';
 import ProjectsPage from '../pages/projects-page';
-import { useSelector } from 'react-redux';
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-  if (!isAuthenticated) {
-    return <Navigate to='/login' replace />;
-  }
-
-  return children;
-};
+import { ProtectedRoute } from './protected-route';
 
 export const router = createBrowserRouter([
   {
